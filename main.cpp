bac6a22ba259b2aacb452affe3eaf9f78dd5167a
unknown <bboismenu@gamingcampus.local>
2023-10-23 14:06:27 +0200

    urgance

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Class/board.hpp"
+#include "Class/tile.hpp"
+
+void main()
+{
+    // Values and utilities
+    int boardDim[2] = {4, 4};
+    Tile* tileList[16];
+
+    Board board(boardDim);
+}
